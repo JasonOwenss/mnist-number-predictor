8f58499e1d1e9b87c98d60a50310f9b675d622de
JasonOwenss <bmarck10@gmail.com>
2020-08-13 15:37:25 +1200

    add python model

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ function App() {
   
 
   function ds(im){
-    let res = [];
+    
     downscale(im,28,28,{imageType:'png'}).then(async function(dataURL){
       setB64(dataURL);
       const tfim = new Image();
       tfim.src = dataURL;
-      const model = await tf.loadLayersModel('./model.json');
+      const model = await tf.loadLayersModel('../CNNmodel/model.json');
       const example = await tf.browser.fromPixels(tfim).mean(2).toFloat().expandDims(0).expandDims(-1).div(255.0);
       const prediction = await model.predict(example);
       //prediction.print()
